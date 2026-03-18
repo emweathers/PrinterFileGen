4aeaf23fbdf6f5116b5d6501bea48a1051e5d5a2
E Weathers <104738787+emweathers@users.noreply.github.com>
2026-03-17 22:48:39 -0600

    Uploaded Gerber to BMP Converter
    Added a converter from Gerber (.gbr) to Bitmap (.bmp) scaled appropriately for use on the printer at a ~59 DPmm size.

---
+++ printer_gbr_converter.py
@@ -0,0 +1,32 @@
+from pygerber.gerberx3.api.v2 import GerberFile, Project, ImageFormatEnum, ColorScheme
+from PIL import Image, ImageEnhance
+import os
+import io
+
+Image.MAX_IMAGE_PIXELS = 462028800
+px_size = 0.017 # In mm
+
+path = "."
+files = [f for f in os.listdir(path) if (os.path.isfile(os.path.join(path,f)) and f.endswith("gbr"))]
+
+border = GerberFile.from_file("chip_tester-Edge_Cuts.gbr")
+imgbuf = io.BytesIO()
+
+os.makedirs("bmp_out", exist_ok=True)
+
+for f in files:
+    dpmmscale = 200
+    temp = GerberFile.from_file(f)
+    imgbuf.seek(0)
+    out = Project([border,temp]).parse().render_raster(imgbuf,dpmm=dpmmscale,image_format=ImageFormatEnum.PNG)
+    imgbuf.seek(0)
+    imgbuf.read()
+    img = Image.open(imgbuf)
+    imgw = int((1/px_size)*img.width/dpmmscale)
+    imgh = int((1/px_size)*img.height/dpmmscale)
+    img = img.resize((imgw,imgh),Image.NEAREST).transpose(Image.FLIP_LEFT_RIGHT)
+    img = ImageEnhance.Contrast(img).enhance(3).convert("1",dither=Image.NONE)
+    img.save("bmp_out/"+f+".bmp")
+    print("Completed image " + f)
+
+
